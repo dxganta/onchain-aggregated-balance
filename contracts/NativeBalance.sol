3c49ce44c4367a58bb78a7d9e45bbe94a7664f3e
realdiganta <digantakalita.ai@gmail.com>
2021-11-13 11:15:50 +0530

    native balance v3 function added

--- contracts/NativeBalance.sol
+++ contracts/NativeBalance.sol
@@ -6,6 +6,8 @@ import "./interfaces/IVotingShare.sol";
 import "../interfaces/uniswapv2/IUniswapV2Pair.sol";
 import "../interfaces/uniswapv3-core/IUniswapV3Pool.sol";
 
+import "./libraries/TickMath.sol";
+
 contract NativeBalance is BoringOwnable {
 
     address public DIGG_VOTING_SHARE = 0x47752Ff9d07EC7803694860566a83356A9eb5f51;
@@ -14,6 +16,8 @@ contract NativeBalance is BoringOwnable {
     address public BADGER_WBTC_SUSHI_POOL = 0x110492b31c59716AC47337E616804E3E3AdC0b4a;
     address public DIGG_WBTC_SUSHI_POOL = 0x9a13867048e01c663ce8Ce2fE0cDAE69Ff9F35E3;
 
+    uint32 public secondsAgo = 1300;
+    uint128 public PRECISION = 10**18;
     /// @notice get the badger & digg balance in wbtc denomination using uniswapV2 pools 
     /// @param _account address of the account for which to get the balance
     function getNativeBalanceV2(address _account) public view returns (uint) {
@@ -25,8 +29,8 @@ contract NativeBalance is BoringOwnable {
         badgerBal = (badgerBal * wbtcReserve * 10**18) / badgerReserve; 
 
         // get digg balance in wbtc denomination from sushi pool
-        (uint wbtcReserve, uint diggReserve, ) = IUniswapV2Pair(DIGG_WBTC_SUSHI_POOL).getReserves();
-        diggBal = (diggBal * wbtcReserve * 10**9) / diggReserve; // digg balance in wbtc denomination
+        (uint wbtcDiggReserve, uint diggReserve, ) = IUniswapV2Pair(DIGG_WBTC_SUSHI_POOL).getReserves();
+        diggBal = (diggBal * wbtcDiggReserve * 10**9) / diggReserve;
 
         return badgerBal + diggBal;
     }
@@ -38,5 +42,20 @@ contract NativeBalance is BoringOwnable {
         uint diggBal = IVotingShare(DIGG_VOTING_SHARE).balanceOf(_account);
 
         // get badger balance in wbtc denomination from uniswap v3 pool
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = secondsAgo;
+        secondsAgos[1] = 0;
+        (int56[] memory tickCumulatives, ) = IUniswapV3Pool(BADGER_WBTC_V3_POOL).observe(secondsAgos);
+
+        int24 twapVal = int24((tickCumulatives[1] - tickCumulatives[0]) / int32(secondsAgo));
+        uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(twapVal); // returns sqrt(1.0001^tick) * 2^96
+        badgerBal = (sqrtPriceX96 / 2**96) ** 2;
+
+        // get digg balance in wbtc denomination from sushi pool
+        (uint wbtcDiggReserve, uint diggReserve, ) = IUniswapV2Pair(DIGG_WBTC_SUSHI_POOL).getReserves();
+        diggBal = (diggBal * wbtcDiggReserve * 10**9) / diggReserve;
+
+        return badgerBal + diggBal;
+
     }
 }
